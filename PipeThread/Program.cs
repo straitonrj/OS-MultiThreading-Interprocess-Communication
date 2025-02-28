6229a85a06b6ea8ac5f26246a4ded38638a461c0
straitonrj <straitonrj@gmail.com>
2025-02-27 21:34:23 -0500

    Added the PipeThread project (IPC part) and its Documents.sln file.

---
+++ PipeThread/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.IO;
+using System.IO.Pipes;
+
+using (var server = new NamedPipeServerStream("ThreadPipe"))
+            {
+                Console.WriteLine("Waiting for connection...");
+                server.WaitForConnection();
+
+                using (var reader = new StreamReader(server))
+                {
+                        string line = reader.ReadToEnd();
+                        Console.WriteLine($"Received from client: {line}");
+                        //Thread.Sleep(30000);
+                }
+            }
